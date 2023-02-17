4db6b6e6c6c9de462f806287a8ea3078a1f387c4
Alex62510 <eagle625@tut.by>
2023-02-17 10:56:28 +0300

    comm

--- VSC/function trueOrFalse(val){.js
+++ VSC/function trueOrFalse(val){.js
@@ -1,11 +1,11 @@
-function trueOrFalse(val){
-    if (val == false)  {  
-      return ("false");             
-    } else if (val == undefined) {
-      return ("false");
-    } else if (val == null) {
-      return ("false");
-    } else {
-      return ("true");
-    }  
-  }
+function trueOrFalse(val) {
+  if (val == false) {
+    return "false";
+  } else if (val == undefined) {
+    return "false";
+  } else if (val == null) {
+    return "false";
+  } else {
+    return "true";
+  } //
+}
