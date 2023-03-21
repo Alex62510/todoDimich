7ec9f0e738ce993defb80161e4f3728f93b07ca3
Alex62510 <eagle625@tut.by>
2023-03-21 11:52:22 +0300

    commit-03 without test addName

--- It-Inkybator/exam-monday-01/my-app/src/App.tsx
+++ It-Inkybator/exam-monday-01/my-app/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UsersList from "./02monday/08";
+
 
 function App() {
   return (
     <div className="App">
-<UsersList/>
+
     </div>
   );
 }
