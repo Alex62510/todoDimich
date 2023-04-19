542d23cabdcf5a4407308d83aec09c1988acdcdc
Alex62510 <eagle625@tut.by>
2023-04-19 15:35:51 +0300

    commit-07 is done

--- It-Inkybator/reactKabzda/my-app/src/components/UncontrolledAccordeon/UncontrolledAccordeon.tsx
+++ It-Inkybator/reactKabzda/my-app/src/components/UncontrolledAccordeon/UncontrolledAccordeon.tsx
@@ -1,32 +1,39 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
+import {reducer, TOGGLE_COLLAPSED} from "./Reducer";
 
 type AccordeonPropsType = {
     titleValue: string
 
 }
+
 export function UncontrolledAccordeon(props: AccordeonPropsType) {
 
-let [collapsed, setCollapsed]=useState(false)
-        return(
+// let [collapsed, setCollapsed]=useState(false)
+    let [state, dispatch] = useReducer(reducer, {collapset:false})
+    return (
         <div>
-            <AccordeonTitle title={props.titleValue} onClick={()=>{setCollapsed(!collapsed)}}/>
-            {collapsed&&<AccordeonBody/>}
+            <AccordeonTitle title={props.titleValue} onClick={() => {dispatch({type: TOGGLE_COLLAPSED})
+            }}/>
+            {state.collapset && <AccordeonBody/>}
         </div>
-        )
+    )
 }
+
 type AccordeonTitlePropstype = {
     title: string
-    onClick:()=>void
+    onClick: () => void
 }
+
 function AccordeonTitle(props: AccordeonTitlePropstype) {
     console.log("AccordeonTitle rendering")
     return (
-        <h3 onClick={()=>props.onClick()}>---{props.title}---</h3>
+        <h3 onClick={() => props.onClick()}>---{props.title}---</h3>
     )
 }
-function AccordeonBody(){
+
+function AccordeonBody() {
     console.log("AccordeonBody rendering")
-    return(
+    return (
         <ul>
             <li>1</li>
             <li>2</li>
