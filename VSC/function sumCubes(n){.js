7a9c55ba03c32adb0232a3a64234fc507d482885
Alex62510 <eagle625@tut.by>
2023-05-30 11:29:21 +0300

    commit-12 is done

--- VSC/function sumCubes(n){.js
+++
@@ -1,7 +0,0 @@
-function sumCubes(n){
-  if (n == 1) {
-    return n**3;
-  } else {
-    return n**3 + sumCubes(n - 1);
-  }
-}
--- VSC/var humanYearsCatYearsDogYears = functio.js
+++
@@ -1,9 +0,0 @@
-var humanYearsCatYearsDogYears = function(humanYears) {
-  let cat = 0;
-  let dog = 0;
-  if (humanYears === 1){cat = 15; dog = 15}
-  else if (humanYears === 2){cat = 24; dog = 24}
-  else {cat = 24 + 4*(humanYears-2); dog = 24 + 5*(humanYears-2)}
-  // Your code here!
-  return [humanYears,cat,dog];
-}
