e82ac7e742bd0ee5c7deda6baaf40c9fb22ab7b7
Alex62510 <eagle625@tut.by>
2023-08-30 21:11:11 +0300

    10 commit

---
+++ It-Inkybator/TodolistDimych/it-incubator-todolist-ts-11/src/state/store.ts
@@ -0,0 +1,14 @@
+import {tasksReducer} from './tasks-reducer';
+import {todolistsReducer} from './todolists-reducer';
+import {combineReducers, legacy_createStore} from 'redux';
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+export const store = legacy_createStore(rootReducer);
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+window.store = store;
